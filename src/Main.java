78791da9f11a3191b834ec2ff28c5bd51533b3ad
79911 <Petrovlistov@gmail.com>
2023-07-14 02:03:11 +0300

    extra parentheses removed

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 13676;
+        int mile;
+        {
+            mile = 20;
+
+            int miles = amount / mile;
+
+            System.out.println("Итоговые мили: " + miles);
+        }
+    }
+}
